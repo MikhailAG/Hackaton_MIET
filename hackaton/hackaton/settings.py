831299a3a1bccc150855d8ce565d29e26e3608cc
Stepan0305 <gorbatenkostepan@gmail.com>
2023-10-19 21:58:21 +0300

    database config

--- hackaton/hackaton/settings.py
+++ hackaton/hackaton/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'hackaton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "miet_hakaton",
+            "USER": "root",
+            "PASSWORD": "qwe123!@#",
     }
 }
 
