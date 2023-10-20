a064f31dcd5b2091faab36e5ed8bab741f5d848d
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-10-21 01:53:49 +0300

    вьюха и вход

---
+++ hackaton/feedback/auth_script.py
@@ -0,0 +1,15 @@
+def authfunc(username, password):
+    from django.apps import apps
+    Users = apps.get_model('feedback', 'Users')
+    for user in Users.objects.all():
+        if username == user.username:
+            if password == user.password:
+                if user.role_id == 1:
+                    return 2 # Данные верны Босс
+                elif user.role_id == 2:
+                    return 3 # Данные верны Тимлид
+                elif user.role_id == 3:
+                    return 4 # Данные верны Сотрудник
+            else:
+                return 1 # Неверный пароль
+    return 0 # Неверный пользователь
--- hackaton/feedback/views.py
+++ hackaton/feedback/views.py
@@ -1,11 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import *
+from django.apps import apps
 
-# Create your views here.
+Users = apps.get_model('feedback', 'Users')
+
 def auth(request):
-    data = Users.objects.all()
+    if request.method == 'POST' and 'login-app' in request.POST:
+        from auth_script import authfunc
+        authfunc(request.username, request.password)
+        return HttpResponseRedirect('worker')
     return render(request, 'auth.html')
 
 def worker(request):
-    data = Users.objects.all()
     return render(request, 'worker.html')
