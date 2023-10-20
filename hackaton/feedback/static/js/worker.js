3d885e779061a2bcd03de9aee4a01e1bcd2ac338
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-10-20 05:00:58 +0300

    TODO

--- hackaton/feedback/static/js/worker.js
+++ hackaton/feedback/static/js/worker.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector(".jsFilter").addEventListener("click", function () {
+    document.querySelector(".jsFilter").addEventListener("click", function () { // Открывашка фильтра
       document.querySelector(".filter-menu").classList.toggle("active");
     });
 
-    var modeSwitch = document.querySelector(".mode-switch");
+    var modeSwitch = document.querySelector(".mode-switch"); // Смена темы
     modeSwitch.addEventListener("click", function () {
       document.documentElement.classList.toggle("dark");
       document.documentElement.classList.toggle("light");
