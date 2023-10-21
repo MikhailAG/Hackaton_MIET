6b92d185fade205904e5ff3e9fc71adf295ff62b
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-10-21 07:24:48 +0300

    Заполнение с БД и изменение открытия page.html

--- hackaton/feedback/static/js/worker.js
+++ hackaton/feedback/static/js/worker.js
@@ -1,4 +1,38 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    const rows_emp = document.querySelectorAll(".products-row.employee");
+    const rows_fb = document.querySelectorAll(".products-row.feedback");
+    const btns_exit_emp = document.querySelectorAll(".btn-exit.employee")
+    const btns_exit_fb = document.querySelectorAll(".btn-exit.feedback")
+
+    rows_emp.forEach(row => {
+        row.addEventListener("click", function() {
+          document.querySelector(".app-content.employee").style.display = "none";
+          document.querySelector(".employee-page").style.display = "block";
+          document.querySelector(".employee-page").scrollTop = 0;
+        });
+    });
+    rows_fb.forEach(row => {
+      row.addEventListener("click", function() {
+          document.querySelector(".app-content.feedback").style.display = "none";
+          document.querySelector(".feedback-page").style.display = "block";
+          document.querySelector(".feedback-page").scrollTop = 0;
+      });
+    });
+
+    btns_exit_emp.forEach(btn => {
+      btn.addEventListener("click", function() {
+          document.querySelector(".app-content.employee").style.display = "flex";
+          document.querySelector(".employee-page").style.display = "none";
+      });
+    });
+    btns_exit_fb.forEach(btn => {
+      btn.addEventListener("click", function() {
+          document.querySelector(".app-content.feedback").style.display = "flex";
+          document.querySelector(".feedback-page").style.display = "none";
+      });
+    });
+
     document.querySelector(".jsFilter").addEventListener("click", function () {
       document.querySelector(".filter-menu").classList.toggle("active");
     });
@@ -19,17 +53,6 @@ window.addEventListener('DOMContentLoaded', () => {
       document.querySelector(".mode-switch").classList.toggle("active");
     });
 
-    document.querySelector(".products-row.employee").addEventListener("click", function () {
-      document.querySelector(".app-content.employee").style.display = "none";
-      document.querySelector(".employee-page").style.display = "block";
-      document.querySelector(".employee-page").scrollTop = 0;
-    });
-    document.querySelector(".products-row.feedback").addEventListener("click", function () {
-      document.querySelector(".app-content.feedback").style.display = "none";
-      document.querySelector(".feedback-page").style.display = "block";
-      document.querySelector(".feedback-page").scrollTop = 0;
-    });
-
     document.querySelector(".sidebar-list-item.feedback").addEventListener("click", function () {
       document.querySelector(".sidebar-list-item.employee").classList.remove("active")
       document.querySelector(".sidebar-list-item.feedback").classList.add("active")
@@ -46,14 +69,4 @@ window.addEventListener('DOMContentLoaded', () => {
       document.querySelector(".employee-page").style.display = "none";
       document.querySelector(".feedback-page").style.display = "none";
     });
-
-    document.querySelector(".btn-exit.employee").addEventListener("click", function () {
-      document.querySelector(".app-content.employee").style.display = "flex";
-      document.querySelector(".employee-page").style.display = "none";
-    });
-    document.querySelector(".btn-exit.feedback").addEventListener("click", function () {
-      document.querySelector(".app-content.feedback").style.display = "flex";
-      document.querySelector(".feedback-page").style.display = "none";
-    });
-
 });
--- hackaton/feedback/views.py
+++ hackaton/feedback/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.apps import apps
 from .scripts.auth_script import authfunc
 from django.conf import settings
 
 Users = apps.get_model('feedback', 'Users')
-
+Feedbacks = apps.get_model('feedback', 'Feedbacks')
 def auth(request):
     if request.method == 'POST' and 'login-app' in request.POST:
         username = request.POST['username']
@@ -14,10 +14,17 @@ def auth(request):
         print('Статус входа:', auth_result)
         if auth_result == 2 or auth_result == 3 or auth_result == 4:
             print('Роль пользователя:', settings.CURRENT_USER.role_id)
+            request.session['role'] = settings.CURRENT_USER.role_id
+            request.session['name'] = settings.CURRENT_USER.name
             return HttpResponseRedirect('worker')
     return render(request, 'auth.html')
 
 def worker(request):
     if isinstance(settings.CURRENT_USER, str):
         return HttpResponseRedirect('/')
-    return render(request, 'worker.html')
+
+    users = Users.objects.all()
+    current_user = settings.CURRENT_USER
+    feedback = Feedbacks.objects.all()
+
+    return render(request, 'worker.html',{'users': users, 'current_user': current_user, 'feedback': feedback})
