0b4e0e0f0ee40c02f63be4fe7757acd37975ad21
MikhailAG <mikhail.gubanov.a@gmail.com>
2023-10-21 03:54:35 +0300

    Вход доделан

--- hackaton/feedback/scripts/auth_script.py
+++ hackaton/feedback/scripts/auth_script.py
@@ -1,9 +1,11 @@
 def authfunc(username, password):
+    from django.conf import settings
     from django.apps import apps
     Users = apps.get_model('feedback', 'Users')
     for user in Users.objects.all():
         if username == user.username:
             if password == user.password:
+                settings.CURRENT_USER = user
                 if user.role_id == 1:
                     return 2 # Данные верны Босс
                 elif user.role_id == 2:
--- hackaton/feedback/views.py
+++ hackaton/feedback/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from .scripts.auth_script import authfunc
+from django.conf import settings
 
 Users = apps.get_model('feedback', 'Users')
 
 def auth(request):
     if request.method == 'POST' and 'login-app' in request.POST:
-        from auth_script import authfunc
-        authfunc(request.username, request.password)
-        return HttpResponseRedirect('worker')
+        username = request.POST['username']
+        password = request.POST['password']
+        auth_result = authfunc(username, password)
+        print('Статус входа:', auth_result)
+        if auth_result == 2 or auth_result == 3 or auth_result == 4:
+            print('Роль пользователя:', settings.CURRENT_USER.role_id)
+            return HttpResponseRedirect('worker')
     return render(request, 'auth.html')
 
 def worker(request):
+    if isinstance(settings.CURRENT_USER, str):
+        return HttpResponseRedirect('/')
     return render(request, 'worker.html')
